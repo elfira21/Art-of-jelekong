5791f484ef6bc50ec371727d01822977ad36f88d
Dimas Aditya <adimasaditya14@gmail.com>
2023-06-23 06:03:04 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'kursus.html'],
   theme: {
     container: {
       center: true,
